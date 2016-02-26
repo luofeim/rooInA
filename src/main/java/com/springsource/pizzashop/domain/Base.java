2ccf0fa3ba31c549a97175e1fc1b4b2cf86deb0f
Kanga Roo <joeys@marsupial.com>
2016-02-26 15:29:14 +0800

    3. create entity

---
+++ src/main/java/com/springsource/pizzashop/domain/Base.java
@@ -0,0 +1,10 @@
+package com.springsource.pizzashop.domain;
+import org.springframework.roo.addon.javabean.annotations.RooJavaBean;
+import org.springframework.roo.addon.javabean.annotations.RooToString;
+import org.springframework.roo.addon.jpa.annotations.activerecord.RooJpaActiveRecord;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class Base {
+}
